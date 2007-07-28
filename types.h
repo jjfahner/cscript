623baeb4c785213d8eb5479ec36a3d707f4ac6a2
jfa <jfa@26fbb16b-c8bc-4b58-a985-33760c0a6288>
2007-07-28 15:35:06 +0000

    Forgot to add types.h

---
+++ types.h
@@ -0,0 +1,11 @@
+#ifndef CSCRIPT_TYPES_H
+#define CSCRIPT_TYPES_H
+
+//
+// Base types
+//
+typedef unsigned __int8   Byte;
+typedef unsigned __int16  Word;
+typedef unsigned __int32  Quad;
+
+#endif // #ifndef CSCRIPT_TYPES_H
