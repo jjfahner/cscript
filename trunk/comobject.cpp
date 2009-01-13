e4768946a308d6e49b457952d47e5e10c404690c
nobody <nobody@26fbb16b-c8bc-4b58-a985-33760c0a6288>
2009-01-13 22:39:58 +0000

    Renamed comclass files to comobject. Also moved their location within the project file.

--- trunk/comobject.cpp
+++ trunk/comobject.cpp
@@ -18,7 +18,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //////////////////////////////////////////////////////////////////////////
-#include "comclass.h"
+#include "comobject.h"
 #include "native.h"
 
 #include <map>
