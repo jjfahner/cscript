94b3d313b09c801b68693d2c831c0d88c0160483
nobody <nobody@26fbb16b-c8bc-4b58-a985-33760c0a6288>
2009-02-16 17:31:04 +0000

    Factoring out native functions from core functionality. Goal is to allow a version of the interpreter that has no native calls supplied by cscript itself. This lets host environments control completely what the language may interface with.

--- trunk/src/native.cpp
+++ trunk/src/native.cpp
@@ -121,10 +121,10 @@ void PrintValue(Value const& val)
     return;
 
   case Value::tInt:       
-  {
-    csout << val.GetInt();
-    return;
-  }
+    {
+      csout << val.GetInt();
+      return;
+    }
 
   case Value::tString:    
     csout << val.GetString();
@@ -168,128 +168,12 @@ NATIVE_CALL("__native print(value)")
   return args[0];
 }
 
-NATIVE_CALL("__native exit(int exitcode = 0)")
-{
-  // Exit with specified exit code
-  exit((int) args[0].GetInt());
-
-  // Never executed
-  throw std::runtime_error("exit() failed");
-}
-
-NATIVE_CALL("__native quit(int exitcode = 0)")
-{
-  // Exit with specified exit code
-  exit((int) args[0].GetInt());
-
-  // Never executed
-  throw std::runtime_error("exit() failed");
-}
-
 NATIVE_CALL("__native read()")
 {
   String line;
   csin >> line;
   return Value(line);
 }
-
-NATIVE_CALL("__native count(arg)")
-{
-  if(args[0].Type() != Value::tObject)
-  {
-    throw std::runtime_error("Invalid type for count");
-  }
-  return args[0]->Count();
-}
-
-struct objprinter {
-  void operator () (Object const* obj) {
-    Value v(const_cast<Object*>(obj));
-    csout << obj->GetTypeName() << " ";
-    PrintValue(v); csout << "\n";
-  }
-};
-
-NATIVE_CALL("__native dump()")
-{
-  std::for_each(Object::GetObjects().begin(), 
-                Object::GetObjects().end(), 
-                objprinter());
-  return Value();
-}
-
-//////////////////////////////////////////////////////////////////////////
-//
-// String functions
-//
-
-NATIVE_CALL("__native strlen(string value)")
-{
-  return Value((Value::Int)args[0].GetString().length());
-}
-
-NATIVE_CALL("__native substr(string data, int start, int len = 0)")
-{
-  String::size_type off = (String::size_type)args[1].GetInt();
-  String::size_type len = (String::size_type)args[2].GetInt();
-  if(len == 0) len = String::npos;
-  return Value(args[0].GetString().substr(off, len));
-}
-
-NATIVE_CALL("__native strstr(string data, string what, int start = 0)")
-{
-  String const& src = args[0].GetString();
-  String const& str = args[1].GetString();
-  int32 offset = (int32)args[2].GetInt();
-  if(str.length() < 1)
-  {
-    throw std::runtime_error("Empty string in call to strstr");
-  }
-  size_t res = src.find(str, offset);
-  if(res == String::npos)
-  {
-    return Value(-1);
-  }
-  return Value((Value::Int)res);
-}
-
-NATIVE_CALL("__native strchr(string data, string char, int start = 0)")
-{
-  String const& src = args["data"].GetString();
-  String const& chr = args["char"].GetString();
-  int32 offset = (int32)args["start"].GetInt();
-  if(chr.length() < 1)
-  {
-    throw std::runtime_error("Empty string in call to strchr");
-  }
-  size_t res = src.find(chr[0], offset);
-  if(res == String::npos)
-  {
-    return Value(-1);
-  }
-  return Value((Value::Int)res);
-}
-
-NATIVE_CALL("__native eval(string code)")
-{
-  return evaluator->Eval(args[0].GetString());
-}
-
-NATIVE_CALL("__native reset()")
-{
-  throw reset_exception();
-}
-
-NATIVE_CALL("__native collect()")
-{
-  evaluator->Collect();
-  return Value();
-}
-
-NATIVE_CALL("__native object_count()")
-{
-  return Object::GetObjects().size();
-};
 
 #if defined(WIN32) && !defined(_WIN32_WCE)
 
