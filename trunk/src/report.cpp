249bb6e0cc7e1cbaa7ee9952957341c3d5a317ab
nobody <nobody@26fbb16b-c8bc-4b58-a985-33760c0a6288>
2009-06-27 10:16:21 +0000

    Cleaned up error handling.

--- trunk/src/report.cpp
+++ trunk/src/report.cpp
@@ -37,36 +37,10 @@ Reporter::Reset()
 }
 
 void 
-Reporter::ReportError(Notice const& notice, FilePos* pos, ...)
-{
-  static const char* szTemplate = "%s (%d) : error E%04d: %s\n";
-
-  // Update error count
-  ++m_errors;
-
-  // Build first generation from template
-  char buf1[4096];
-  sprintf(buf1, szTemplate, 
-    pos->m_file.c_str(), pos->m_line, 
-    notice.m_number, notice.m_notice);
-
-  // Prepare arglist
-  va_list args;
-  va_start(args, pos);
-
-  // Build second generation from first and variadic args
-  char buf2[8192];
-  vsprintf(buf2, buf1, args);
-
-  // Write string
-  std::cout << buf2;
-}
-
-void 
-Reporter::ReportWarning(FilePos& pos, String const& text)
+Reporter::ReportWarning(String const& text)
 {
   ++m_warnings;
-  std::cout << pos.m_file << " (" << pos.m_line << ") : warning: " << text << "\n";
+  std::cout << "warning: " << text << "\n";
 }
 
 int 
