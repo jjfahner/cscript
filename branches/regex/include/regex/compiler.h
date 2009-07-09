3434c7e74ef2355beaeb60079ea6feb3728de068
nobody <nobody@26fbb16b-c8bc-4b58-a985-33760c0a6288>
2009-07-09 22:14:49 +0000

    - Cleaned up compiler header - Sorted compiler sources to allow inlining of AddXXX methods - Added some special chars to the atom list.

--- branches/regex/include/regex/compiler.h
+++ branches/regex/include/regex/compiler.h
@@ -18,8 +18,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //////////////////////////////////////////////////////////////////////////
-#ifndef CSCRIPT_RE_H
-#define CSCRIPT_RE_H
+#ifndef CSCRIPT_REGEX_COMPILER_H
+#define CSCRIPT_REGEX_COMPILER_H
 
 #include <cscript.h>
 #include <object.h>
@@ -28,13 +28,6 @@
 #include <vector>
 
 class LexStream;
-
-enum SpecialChars
-{
-  StartAnchor = 1,
-  EndAnchor = 2,
-  AnyChar = 3
-};
 
 enum TransitionTypes
 {
@@ -82,7 +75,7 @@ typedef std::vector<size_t> SizeVec;
 typedef std::vector<SizeVec> TransitionTable;
 
 //
-// Compiler context
+// Regular expression compiler
 //
 class RegexCompiler
 {
@@ -187,177 +180,4 @@ public:
 
 };
 
-struct Frame
-{
-  void Set(State state = 0, char const* start = 0, char const* ptr = 0, Frame* next = 0)
-  {
-    m_state = state;
-    m_start = start;
-    m_ptr   = ptr;
-    m_next  = next;
-  }
-
-  State       m_state;
-  char const* m_start;
-  char const* m_ptr;
-  Frame*      m_next;
-
-};
-
-////////////////////////////////////////////////////////////////////////
-
-// namespace Ast
-// {
-//   class Expression : public Object
-//   {
-//   public:
-// 
-//     Expression() :
-//     m_min (1),
-//     m_max (1)
-//     {
-//     }
-// 
-//     void SetQuantifier(Pair const& q)
-//     {
-//       m_min = q.m_min;
-//       m_max = q.m_max;
-//     }
-// 
-//     virtual Pair Compile(CompileContext&) = 0;
-// 
-//   protected:
-// 
-//     int m_min;
-//     int m_max;
-// 
-//   };
-// 
-//   //////////////////////////////////////////////////////////////////////
-// 
-//   class Alternation : public Expression
-//   {
-//   public:
-// 
-//     Alternation(Expression* lhs, Expression* rhs) :
-//     m_lhs (lhs),
-//     m_rhs (rhs)
-//     {
-//     }
-// 
-//     virtual Pair Compile(CompileContext&);
-// 
-//   protected:
-// 
-//     virtual void MarkObjects(GCObjectVec& grey)
-//     {
-//       Expression::MarkObjects(grey);
-//       GC::Mark(grey, m_lhs);
-//       GC::Mark(grey, m_rhs);
-//     }
-// 
-//     Expression* m_lhs;
-//     Expression* m_rhs;
-// 
-//   };
-// 
-//   //////////////////////////////////////////////////////////////////////
-// 
-//   class Sequence : public Expression
-//   {
-//   public:
-// 
-//     void Append(Expression* expression)
-//     {
-//       m_expressions.push_back(expression);
-//     }
-// 
-//     virtual Pair Compile(CompileContext&);
-// 
-//   protected:
-// 
-//     typedef std::vector<Expression*> Expressions;
-// 
-//     virtual void MarkObjects(GCObjectVec& grey)
-//     {
-//       Expression::MarkObjects(grey);
-// 
-//       Expressions::iterator it, ie;
-//       it = m_expressions.begin();
-//       ie = m_expressions.end();
-// 
-//       for(; it != ie; ++it)
-//       {
-//         GC::Mark(grey, *it);
-//       }
-//     }
-// 
-//     Expressions m_expressions;
-// 
-//   };
-// 
-//   //////////////////////////////////////////////////////////////////////
-// 
-//   class Range : public Expression
-//   {
-//   public:
-// 
-//     Range() : m_invert (false)
-//     {
-//     }
-// 
-//     void Append(Pair const& part)
-//     {
-//       m_parts.push_back(part);
-//     }
-// 
-//     void Invert(bool invert)
-//     {
-//       m_invert = invert;
-//     }
-// 
-//     virtual Pair Compile(CompileContext&);
-// 
-//   protected:
-// 
-//     typedef std::vector<Pair> Parts;
-// 
-//     Parts m_parts;
-//     bool m_invert;
-// 
-//   };
-// 
-//   //////////////////////////////////////////////////////////////////////
-// 
-//   class Char : public Expression
-//   {
-//   public:
-// 
-//     Char(char value) : 
-//     m_value (value)
-//     {
-//     }
-// 
-//     virtual Pair Compile(CompileContext&);
-// 
-//   protected:
-// 
-//     char m_value;
-// 
-//   };
-// 
-// 
-// } // namespace Ast
-// 
-// //
-// // Compile a regular expression
-// //
-// void Compile(Ast::Expression*);
-// 
-// //
-// // Match a regular expression
-// //
-// void Match(CompileContext const& ctx, char const* text);
-
-
-#endif // CSCRIPT_RE_H
+#endif // CSCRIPT_REGEX_COMPILER_H
